80d0a0692415de48dac7b72ea6db1cd00bc29dc4
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-09 06:29:59 +0000

    Interfaces Completed

---
+++ Interfaces/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            myCar.Move();
+            myBicycle.Move();
+        }
+    }
+}
